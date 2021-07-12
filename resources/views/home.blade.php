5a42d44bbdff8bbdca8b3f08a13034e19aceaf23
mattiadebonis <mattia.debonis@gmail.com>
2021-07-13 01:23:13 +0200

    Esercizio completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,6 +64,10 @@
         </style>
     </head>
     <body>
+
+        <?php
+            $prova = ["Primo", "esercizio","con", "Laravel"];
+        ?>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -81,19 +85,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello World
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                
+                <ul>
+                    @foreach ($prova as $value)    
+                        <p>{{$value}}</p>
+                    @endforeach
+                </ul>
+                    
+                
             </div>
         </div>
     </body>
